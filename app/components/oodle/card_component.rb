8e5226368d1ad5bdc8166a021e23c377f62d8590
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-10-20 20:59:36 +0200

    Update show and index views for answers, items, questionnaires and questions

--- app/components/oodle/card_component.rb
+++ app/components/oodle/card_component.rb
@@ -4,9 +4,10 @@ module Oodle
   class CardComponent < ViewComponent::Base
     include Turbo::FramesHelper
 
-    def initialize(title: nil, id: nil, &block)
+    def initialize(title: nil, id: nil, actions: [], &block)
       @title = title
       @id = id
+      @actions = actions
     end
   end
 end
