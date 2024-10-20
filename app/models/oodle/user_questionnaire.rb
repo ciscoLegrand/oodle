30ad0531b89b8802e2664c0079edaf4dea3b002b
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-10-20 20:33:06 +0200

    Build user questionnaire methods

--- app/models/oodle/user_questionnaire.rb
+++ app/models/oodle/user_questionnaire.rb
@@ -4,5 +4,21 @@ module Oodle
     belongs_to :questionnaire, class_name: "Oodle::Questionnaire"
 
     has_many :answers, through: :questionnaire
+
+    scope :completed, -> { where(completed: true, progress: 100) }
+    scope :incompleted, -> { where(completed: false).where("progress < ?", 100) }
+
+    def update_progress = update! progress: ((answers.count.to_f / questionnaire.questions.count) * 100)
+
+    def subscribed? = persisted?
+
+    def manager_owner? = questionnaire.manager = user
+
+    def subscribe
+      return errors.addd(:user, "managers can´t subscribe to questionnaires") if manager_owner?
+      return errors.addd(:user, "already subscribed to this questionnaire") if subscribed?
+
+      save user_id: user.id, questionnaire_id: questionnaire.id, start_time: Time.zone.now
+    end
   end
 end
