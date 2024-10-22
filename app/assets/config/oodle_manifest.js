93f18cbbc008d11bb39cdb23ff06c53a5791ed75
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-10-22 13:06:28 +0200

    Change layout for using engine layout and add tailwind custom classes

--- app/assets/config/oodle_manifest.js
+++ app/assets/config/oodle_manifest.js
@@ -1,3 +1,4 @@
+// oodle_manifest.js
 //= link_tree ../builds/ .css
-//= link oodle/application.css
 //= link oodle/application.tailwind.css
+
--- app/controllers/oodle/application_controller.rb
+++ app/controllers/oodle/application_controller.rb
@@ -1,6 +1,8 @@
 module Oodle
-  class ApplicationController < ::ApplicationController
-    layout "application"
+  class ApplicationController < ::ApplicationController # Hereda del ApplicationController principal
+    layout "oodle/application" # Sigue usando el layout de la engine
+
+    # Incluir los helpers de la aplicación principal
     helper Rails.application.helpers
   end
 end
